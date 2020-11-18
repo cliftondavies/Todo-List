216a2bffed1ad141f28ba51d8508edac873ff68b
cliftondavies <clifton.davies@outlook.com>
2020-11-18 20:32:01 +0000

    Refactor retrieve() and define removeProject()

--- src/storage.js
+++ src/storage.js
@@ -3,13 +3,11 @@ import Project from './project';
 const Storage = class {
   // retrieve todo list (either all the todos or just the todos in a particular project category)
   static retrieve() {
-    if (JSON.parse(localStorage.getItem('list'))) {
-      JSON.parse(localStorage.getItem('list'));
-    } else {
+    if (JSON.parse(localStorage.getItem('list')) === null) {
       const defaultProject = [new Project()];
       localStorage.setItem('list', JSON.stringify(defaultProject));
-      JSON.parse(localStorage.getItem('list'));
     }
+    return JSON.parse(localStorage.getItem('list'));
   }
 
   // save (project or todo object) to storage
@@ -18,6 +16,7 @@ const Storage = class {
 
     if (obj instanceof Project) {
       storedProjects.push(obj);
+      // Call Project.addCategory(obj) here?
     } else {
       const project = storedProjects.find(project => project.projectName === obj.category);
       const index = storedProjects.indexOf(project);
@@ -28,43 +27,17 @@ const Storage = class {
     localStorage.setItem('list', JSON.stringify(storedProjects));
   }
 
-  // remove todo from a project list (implement here or in project class)
+  // remove project
+  static removeProject(projects, index) {
+    projects.splice(index, 1);
+    localStorage.setItem('list', JSON.stringify(projects));
+  }
 
-  // remove a project from the storage (nice to have)
+  // remove todo from the storage (here or in todo)
+  static removeTodo() {
 
+  }
 
-  // const todolist = [
-  //   {
-  //     title: 'work',
-  //     list_of_todos: [{
-  //       title: 'todo-title1',
-  //       description: 'todo description1',
-  //       dueDate: 'todo due date',
-  //       priority: 'todo-priority',
-  //     },
-  //     {
-  //       title: 'todo-title2',
-  //       description: 'todo description2',
-  //       dueDate: 'todo due date',
-  //       priority: 'todo-priority',
-  //     }],
-  //   },
-  //   {
-  //     title: 'shopping',
-  //     list_of_todos: [{
-  //       title: 'todo-title',
-  //       description: 'todo description',
-  //       dueDate: 'todo due date',
-  //       priority: 'todo-priority',
-  //     },
-  //     {
-  //       title: 'todo-title',
-  //       description: 'todo description',
-  //       dueDate: 'todo due date',
-  //       priority: 'todo-priority',
-  //     }],
-  //   },
-  // ];
 };
 
 export { Storage as default };
