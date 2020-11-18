a5cc9fa3378ab0a1da2b9566313a341edc5f8ae4
cliftondavies <clifton.davies@outlook.com>
2020-11-18 20:24:30 +0000

    Add event listeners

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import './assets/css/style.css';
 import content from './content';
+import UI from './ui';
 
 content().staticPage();
 content().todoForm();
 content().projectForm();
+
+document.addEventListener('DOMContentLoaded', UI.render);
+
+const viewProjectFormBtn = document.querySelector('.createProjectBtn');
+const viewTodoFormBtn = document.querySelector('.createTodoBtn');
+const projectForm = document.querySelector('.project-form');
+const todoForm = document.querySelector('.todo-form');
+
+viewProjectFormBtn.addEventListener('click', UI.viewProjectForm);
+viewTodoFormBtn.addEventListener('click', UI.viewTodoForm);
+projectForm.addEventListener('submit', UI.newCategory);
+todoForm.addEventListener('submit', UI.newTodo);
