8ac1e73872b76abd119f70b57bf70dd7d5a846fa
cliftondavies <clifton.davies@outlook.com>
2020-11-20 20:09:27 +0000

    Set todo id to string on todo creation

--- src/todo.js
+++ src/todo.js
@@ -6,14 +6,15 @@ const Todo = class {
     this.priority = priority;
     this.category = category;
     this.completed = 'Incomplete';
-    this.id = Todo.generateId();
+    // this.id = Todo.generateId();
+    this.id = Date.now().toString(); // returns a number so call toString() here
   }
 
-  static numberOfTodos = 0 // to implement otherwise might be a problem when refreshed
+  // static numberOfTodos = 0 // to implement otherwise might be a problem when refreshed
 
-  static generateId() {
-    return Todo.numberOfTodos + 1;
-  }
+  // static generateId() {
+  //   return Todo.numberOfTodos + 1;
+  // }
 
   // toggleCompleteStatus(){}
   updateStatus() {
