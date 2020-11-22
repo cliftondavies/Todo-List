ad2ee1e10590a35018104ec299f036c08f7de418
cliftondavies <clifton.davies@outlook.com>
2020-11-22 22:46:41 +0000

    Change from class to a module

--- src/storage.js
+++ src/storage.js
@@ -1,22 +1,22 @@
 import Project from './project';
 
-const Storage = class {
+const storage = (() => {
   // retrieve todo list (either all the todos or just the todos in a particular project category)
-  static retrieve() {
+  const retrieve = () => {
     if (JSON.parse(localStorage.getItem('list')) === null) {
       const defaultProject = [new Project()];
       localStorage.setItem('list', JSON.stringify(defaultProject));
     }
     return JSON.parse(localStorage.getItem('list'));
-  }
+  };
 
   // save new (project or todo object) to storage
-  static save(obj, index = null) {
-    const storedProjects = this.retrieve();
+  const save = (obj, index = null) => {
+    const storedProjects = storage.retrieve();
 
     if (index) { // or Project obj && index if this doesn't work
       storedProjects[index] = obj;
-    } else if (obj instanceof Project) { // Project obj && !index
+    } else if (obj instanceof Project) { // Project obj && !index // also check obj binding
       storedProjects.push(obj);
     } else {
       const project = storedProjects.find(project => project.projectName === obj.category);
@@ -27,7 +27,7 @@ const Storage = class {
     }
 
     localStorage.setItem('list', JSON.stringify(storedProjects));
-  }
+  };
 
   // static update(projects, index, project) {
   //   projects[index] = project;
@@ -35,10 +35,16 @@ const Storage = class {
   // }
 
   // remove project
-  static removeProject(projects, index) {
+  const removeProject = (projects, index) => {
     projects.splice(index, 1);
     localStorage.setItem('list', JSON.stringify(projects));
-  }
-};
+  };
 
-export { Storage as default };
+  return {
+    retrieve,
+    save,
+    removeProject,
+  };
+})();
+
+export { storage as default };
