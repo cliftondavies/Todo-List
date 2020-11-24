d85855b05b753f3e999868ac10a68afa47f47de7
cliftondavies <clifton.davies@outlook.com>
2020-11-24 22:21:37 +0000

    Fix issues when saving to storage

--- src/storage.js
+++ src/storage.js
@@ -1,7 +1,7 @@
 import Project from './project';
 
 const storage = (() => {
-  // retrieve todo list (either all the todos or just the todos in a particular project category)
+  // retrieve all projects ans todosfrom storage
   const retrieve = () => {
     if (JSON.parse(localStorage.getItem('list')) === null) {
       const defaultProject = [new Project()];
@@ -10,31 +10,27 @@ const storage = (() => {
     return JSON.parse(localStorage.getItem('list'));
   };
 
-  // save new (project or todo object) to storage
+  // update the storage with a project or todo item
   const save = (obj, index = null) => {
     const storedProjects = storage.retrieve();
 
-    if (index) { // or Project obj && index if this doesn't work
+    if (index || index === 0) {
       storedProjects[index] = obj;
-    } else if (obj instanceof Project) { // Project obj && !index // also check obj binding
+    } else if (obj instanceof Project) {
       storedProjects.push(obj);
     } else {
       const project = storedProjects.find(project => project.projectName === obj.category);
       const index = storedProjects.indexOf(project);
-      // project.list.push(todo);
-      project.saveTodo(obj); // won't work if project is null;
-      storedProjects[index] = project;
+      const projectCopy = Object.assign(new Project(), project);
+
+      projectCopy.saveTodo(obj);
+      storedProjects[index] = projectCopy;
     }
 
     localStorage.setItem('list', JSON.stringify(storedProjects));
   };
 
-  // static update(projects, index, project) {
-  //   projects[index] = project;
-  //   localStorage.setItem('list', JSON.stringify(projects));
-  // }
-
-  // remove project
+  // remove project from storage
   const removeProject = (projects, index) => {
     projects.splice(index, 1);
     localStorage.setItem('list', JSON.stringify(projects));
